0d0859edcd80c0f0737ffa4cc2fbd416bacaaccf
Radoslaw Medryk <radoslaw.medryk@yhoo.com>
2019-04-30 22:23:28 +0200

    Added configs for different envs

--- src/config.tsx
+++ src/config.tsx
@@ -4,6 +4,7 @@ export const configPromise = axios
     .get("/config.json")
     .then(response => {
         const config = response.data;
+
         if (!config || !config.apiBaseUrl || typeof config.apiBaseUrl !== "string") {
             throw new Error("Unsupported response.data format.");
         }
@@ -12,7 +13,8 @@ export const configPromise = axios
             apiBaseUrl: config.apiBaseUrl as string,
         };
     })
-    .catch(() => {
+    .catch(e => {
+        console.warn("Get /config.json failed, fallback to defaults.", e);
         return {
             // apiBaseUrl: "https://bank-core.herokuapp.com",
             apiBaseUrl: "http://localhost:5000",
