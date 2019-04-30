e5968cd85940788168c6e18086085f10913ac3a3
Radoslaw Medryk <radoslaw.medryk@yhoo.com>
2019-04-30 22:45:59 +0200

    WIP - Started work on TransferPage

--- src/pages/TransferPage.tsx
+++ src/pages/TransferPage.tsx
@@ -3,12 +3,67 @@ import { PageProps } from "./PageProps";
 import { PageBox } from "./PageBox";
 import { MobileMenuContainer } from "src/containers/MobileMenuContainer";
 import { AuthRedirect } from "src/containers/AuthRedirect";
+import { AccountSwitcherContainer } from "src/containers/AccountSwitcherContainer";
+import { styled } from "linaria/react";
+import { Option } from "src/components/Option";
+import { Icon } from "src/components/Icon";
+import { Section } from "src/components/Section";
+import { Link } from "react-router-dom";
 
-export const TransferPage: React.SFC<PageProps> = ({}) => {
+const Spacer = styled.div`
+    height: 52px;
+`;
+
+const OptionsList = styled.div`
+    margin: 30px 0 0;
+
+    & > a {
+        display: block;
+
+        &:link,
+        &:visited,
+        &:hover,
+        &:active {
+            color: unset;
+        }
+
+        &:not(:first-child) {
+            margin: 30px 0 0;
+        }
+    }
+`;
+
+export const TransferPage: React.SFC<PageProps> = ({ match }) => {
     return (
         <PageBox>
             <AuthRedirect to="/login" when="not-logged-in" />
-            <div>TransferPage</div>
+            <Spacer />
+            <AccountSwitcherContainer />
+            <Section>
+                <OptionsList>
+                    <Link to={`${match.url}/friend`}>
+                        <Option
+                            icon={<Icon type="Contacts" />}
+                            title="To a friend"
+                            description="Transfer funds to a friend that already has an account"
+                        />
+                    </Link>
+                    <Link to={`${match.url}/bank`}>
+                        <Option
+                            icon={<Icon type="Bank" />}
+                            title="To bank account"
+                            description="Transfer funds to bank account as SEPA / SWIFT transfer"
+                        />
+                    </Link>
+                    <Link to={`${match.url}/email`}>
+                        <Option
+                            icon={<Icon type="Email" />}
+                            title="To email address"
+                            description="Recipient will recieve email with instructions how to accept transfer"
+                        />
+                    </Link>
+                </OptionsList>
+            </Section>
             <MobileMenuContainer />
         </PageBox>
     );
